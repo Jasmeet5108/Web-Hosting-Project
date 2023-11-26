838f1caa2c1937619389f0e356f251e4827ac61c
Jasmeet Singh <jasmeet5108k@gmail.com>
2023-11-26 20:11:11 +0530

    Added some functionality to Hamburger menu and Side menu.

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let hamburger = document.querySelector("#hamburger");
-let sideMenu = document.querySelector("#side-menu");
+let sideMenu = document.querySelector(".side-menu");
+let firstLine = document.querySelector(".firstLine");
+let secondLine = document.querySelector(".secondLine");
+let thirdLine = document.querySelector(".thirdLine");
 
 hamburger.addEventListener('click', () => {
-    sideMenu.classList.toggle(".open");
+    sideMenu.classList.toggle('open');
+    firstLine.classList.toggle('rotate45');
+    secondLine.classList.toggle('hidden');
+    thirdLine.classList.toggle('rotate315');
 })
